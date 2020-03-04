88ad3e63d9ae68ee0c91d70b1f676cc86df366e2
Yoshihiro Seki <nikochan2k@gmail.com>
2020-03-04 22:53:47 +0900

    update packages

---
+++ src/__tests__/expo-fs_index.spec.ts
@@ -0,0 +1,5 @@
+import { testAll } from "kura";
+import { ExpoFsLocalFileSystemAsync } from "../expo-fs";
+
+const factory = new ExpoFsLocalFileSystemAsync("web-file-system-test", true);
+testAll(factory);
