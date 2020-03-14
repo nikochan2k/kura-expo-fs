7fd80913f1f99ce7d498e3986555e66e2f56899f
Yoshihiro Seki <nikochan2k@gmail.com>
2020-03-14 10:11:11 +0900

    fix testcase

--- src/__tests__/expo-fs.spec.ts
+++ src/__tests__/expo-fs.spec.ts
@@ -1,4 +1,4 @@
-import { testAll } from "kura";
+import { testAll } from "../../node_modules/kura/lib/__tests__/filesystem";
 import { ExpoFsLocalFileSystemAsync } from "../expo-fs";
 
 const factory = new ExpoFsLocalFileSystemAsync("web-file-system-test", {
