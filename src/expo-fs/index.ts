35dc06a2f269f043174b73d9ec001abca994b4bb
Yoshihiro Seki <nikochan2k@gmail.com>
2020-03-20 18:25:46 +0900

    modify index.ts for react native

--- src/expo-fs/index.ts
+++ src/expo-fs/index.ts
@@ -1,29 +1,31 @@
 import { decode } from "base-64";
 import { dataUriToBase64, fileReaderSingleton } from "kura";
 
-global.Buffer = global.Buffer || require("buffer").Buffer;
+if (navigator && navigator.product == "ReactNative") {
+  global.Buffer = global.Buffer || require("buffer").Buffer;
 
-(process as any).browser = true;
+  (process as any).browser = true;
 
-FileReader.prototype.readAsArrayBuffer = function(blob) {
-  if (this.readyState === this.LOADING) throw new Error("InvalidStateError");
-  (this as any)._setReadyState(this.LOADING);
-  (this as any)._result = null;
-  (this as any)._error = null;
-  const reader = fileReaderSingleton;
-  reader.onloadend = () => {
-    const base64 = dataUriToBase64(reader.result as string);
-    const content = decode(base64);
-    const buffer = new ArrayBuffer(content.length);
-    const view = new Uint8Array(buffer);
-    view.set(Array.from(content).map(c => c.charCodeAt(0)));
-    (this as any)._result = buffer;
-    (this as any)._setReadyState(this.DONE);
+  FileReader.prototype.readAsArrayBuffer = function(blob) {
+    if (this.readyState === this.LOADING) throw new Error("InvalidStateError");
+    (this as any)._setReadyState(this.LOADING);
+    (this as any)._result = null;
+    (this as any)._error = null;
+    const reader = fileReaderSingleton;
+    reader.onloadend = () => {
+      const base64 = dataUriToBase64(reader.result as string);
+      const content = decode(base64);
+      const buffer = new ArrayBuffer(content.length);
+      const view = new Uint8Array(buffer);
+      view.set(Array.from(content).map(c => c.charCodeAt(0)));
+      (this as any)._result = buffer;
+      (this as any)._setReadyState(this.DONE);
+    };
+    setTimeout(() => {
+      reader.readAsDataURL(blob);
+    }, 0);
   };
-  setTimeout(() => {
-    reader.readAsDataURL(blob);
-  }, 0);
-};
+}
 
 export * from "./ExpoFsLocalFileSystem";
 export * from "./ExpoFsLocalFileSystemAsync";
