a7c4c870e9afb51069e37cc7d14df1d9b1ca2289
Yoshihiro Seki <yoshihiro@YoshihironoMacBook-Pro.local>
2020-10-11 23:27:56 +0900

    update packages

--- src/__tests__/expo-fs_index.spec.ts
+++ src/__tests__/expo-fs_index.spec.ts
@@ -4,9 +4,6 @@ import { ExpoFsLocalFileSystemAsync } from "../expo-fs";
 
 const factory = new ExpoFsLocalFileSystemAsync("web-file-system-test", {
   index: true,
-  indexOptions: {
-    writeDelayMillis: 0,
-  },
   verbose: true,
 });
 testAll(factory);
