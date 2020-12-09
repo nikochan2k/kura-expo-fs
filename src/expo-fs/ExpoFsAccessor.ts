59414d5dc75b5841fe2b1078aae81c51c0e32588
関 慶宏 <seki@ifx.local>
2020-12-09 13:38:33 +0900

    modify lastModified to be integer

--- src/expo-fs/ExpoFsAccessor.ts
+++ src/expo-fs/ExpoFsAccessor.ts
@@ -74,7 +74,7 @@ export class ExpoFsAccessor extends AbstractAccessor {
     return {
       fullPath: fullPath,
       name: fullPath.split(DIR_SEPARATOR).pop(),
-      lastModified: info.modificationTime * 1000,
+      lastModified: Math.floor(info.modificationTime * 1000),
       size: info.isDirectory ? undefined : info.size,
     };
   }
@@ -102,7 +102,7 @@ export class ExpoFsAccessor extends AbstractAccessor {
       objects.push({
         fullPath: fullPath,
         name: name,
-        lastModified: info.modificationTime * 1000,
+        lastModified: Math.floor(info.modificationTime * 1000),
         size: info.isDirectory ? undefined : info.size,
       });
     }
